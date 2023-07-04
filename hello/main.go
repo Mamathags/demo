2e24961b8d74ca4a0424f94c66810815ee1cb07b
Mamatha G S <mamathags@rvce.edu.in>
2023-07-04 16:14:07 +0530

    Update main.go

--- hello/main.go
+++ hello/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, 世界")
+	fmt.Fprintln(w, "Hello, Welcome to the world of Cloud native development")
 }
 
 func main() {
